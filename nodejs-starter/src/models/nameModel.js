b0c821888e6a53b8d4c479461157e286436e80b5
Yasir Hassan <156187159+Yasir-Hasann@users.noreply.github.com>
2024-05-25 17:24:22 +0500

    Update nameModel.js

--- nodejs-starter/src/models/nameModel.js
+++ nodejs-starter/src/models/nameModel.js
@@ -5,6 +5,7 @@ const nameSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true, lowercase: true },
+    password: { type: String, required: true, select: false },
   },
   {
     timestamps: true
