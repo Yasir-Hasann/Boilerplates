cb5d2e7dd01e79c842cd0e7dd80866fc6134aba1
Yasir Hassan <engr.yasirhassan@gmail.com>
2025-07-11 12:14:46 +0500

    test husky

--- nodejs-starter/server.js
+++ nodejs-starter/server.js
@@ -1,13 +1,16 @@
 // file imports
+require('./src/config/env-config');
 const app = require('./src/app');
 const connectDB = require('./src/config/database');
-require('./src/config/env-config');
 
 // variable initialization
 const port = process.env.PORT || 5001;
 
 // connect to database
 connectDB();
+
+let a = '122';
+a = 13;
 
 app.listen(port, () => {
   console.log(`🚀 Server is running on port: ${port}`);
